eb3529e6962766ee2eb1b6d8c19e76a7f7ced0c6
kashif56 <8bpcoins4u@gmail.com>
2025-02-17 23:05:46 +0500

    Fixed

--- frontend/src/components/LandingPage/Hero.jsx
+++ frontend/src/components/LandingPage/Hero.jsx
@@ -39,14 +39,14 @@ const Hero = () => {
               <p className="mt-3 max-w-md mx-auto text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl font-poppins">
                 Schedule posts, analyze performance, and grow your Instagram following with AI-powered tools. The all-in-one Instagram management platform for creators and businesses.
               </p>
-              <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+              <div className="mt-5 w-full mx-auto sm:flex sm:justify-center md:mt-8">
                 <div className="rounded-md shadow">
                   {accessToken ? (
                     <Link
                     to='/posts/add'
                     className="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 md:py-4 md:text-lg md:px-10 transition-all duration-300 transform hover:scale-105"
                     >
-                    <span className='flex items-center gap-1'><AiOutlineRight /> Schedule Posts</span>
+                    <span className='flex items-center gap-1'><AiOutlineRight />Schedule Posts</span>
                     
                   </Link>
                   ) : (
